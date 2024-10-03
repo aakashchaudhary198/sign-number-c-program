1889f0084043728324e38812ce700b6080c2e55c
aakashchaudhary198 <aakashchoudhary016@gmail.com>
2024-10-03 12:06:16 +0530

    Add files via upload

---
+++ signnumber.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int number;
+	printf("enter the value of number \n");
+	scanf("%d",&number);
+	if(number>0){
+	
+	printf("the number is positive");
+}
+
+else if(number<0)
+{
+printf("the number is negative");
+}
+else {
+	printf(" the number is zero");
+}
+
+	return 0;
+}
